390b20265dfce1d74736b70ebb64b7ba74e13488
Nguyen Hai Anh <haianh230797@gmail.com>
2020-09-24 17:57:12 +0700

    add function send image

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import {
-  Text,
-  View,
-  SafeAreaView,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-} from 'react-native';
-import {FlatList, TextInput, Platform} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {
   GiftedChat,
   Actions,
   Composer,
   Send,
   InputToolbar,
-  ActionsProps,
 } from 'react-native-gifted-chat';
 import {Icon} from 'native-base';
 import io from 'socket.io-client';
 import ImagePicker from 'react-native-image-picker';
 
 const App = () => {
-  const [listChat, setListChat] = useState([]);
+  const [listChat, setListChat] = useState([
+    {
+      _id: 1,
+      text: 'Hello developer',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'React Native',
+        avatar: 'https://placeimg.com/140/140/any',
+      },
+      image:
+        'https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
+    },
+  ]);
+
   const [text, setText] = useState('');
-  const [image, setImage] = useState();
+
   let socket = io('http://10.10.10.53:3000');
-  console.log(image ? 'image src : ' + image : 'no data');
 
   const options = {
     title: 'Select Avatar',
@@ -57,17 +60,13 @@ const App = () => {
   const renderInputToolbar = (props) => (
     <InputToolbar
       {...props}
-      containerStyle={{
-        backgroundColor: '#222B45',
-        paddingTop: 6,
-      }}
+      containerStyle={styles.containerToolbar}
       primaryStyle={{alignItems: 'center'}}
     />
   );
+
   const pickImage = () => {
     ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', Object.keys(response));
-
       if (response.didCancel) {
         console.log('User cancelled image picker');
       } else if (response.error) {
@@ -75,38 +74,27 @@ const App = () => {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        const source = {uri: response.uri};
-
-        // You can also display the image using data:
-        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-        setImage({
-          avatarSource: source.avatarSource,
-        });
+        const source = {uri: 'data:image/png;base64,' + response.data};
+        let messContent = {
+          _id: Math.random() * 10000000000000000000000000000000,
+          createdAt: new Date(),
+          user: {
+            _id: 1,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+          image: source.uri,
+        };
+        socket.emit('chat message', messContent);
+        setListChat(GiftedChat.append(listChat, messContent));
       }
     });
-    // const ImageMess = {
-    //   _id: Math.random() * 1000000000000000000000000000000,
-    //   createdAt: '2020-09-23T04:39:06.039Z',
-    //   text: image,
-    //   user: {_id: 1},
-    // };
-    // socket.emit('chat message', ImageMess);
-    // setListChat(GiftedChat.append(listChat, ImageMess));
   };
 
   const renderActions = (props) => (
     <Actions
       {...props}
-      containerStyle={{
-        width: 44,
-        height: 44,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 4,
-        marginRight: 4,
-        marginBottom: 0,
-      }}
+      containerStyle={styles.actionContainer}
       icon={() => (
         <Icon name="options" type="SimpleLineIcons" style={{color: 'white'}} />
       )}
@@ -124,53 +112,64 @@ const App = () => {
   );
 
   const renderComposer = (props) => (
-    <Composer
-      {...props}
-      textInputStyle={{
-        color: '#222B45',
-        backgroundColor: '#EDF1F7',
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#E4E9F2',
-        paddingTop: 8.5,
-        paddingHorizontal: 12,
-        marginLeft: 0,
-      }}
-    />
+    <Composer {...props} textInputStyle={styles.textInputBox} />
   );
 
   const renderSend = (props) => (
     <Send
       {...props}
       disabled={!props.text}
-      containerStyle={{
-        width: 44,
-        height: 44,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginHorizontal: 4,
-      }}>
+      containerStyle={styles.containerSend}>
       <Icon name="send" type="Feather" style={{color: 'white'}} />
     </Send>
   );
 
   return (
-    <React.Fragment>
-      {/* {image && <Image source={image.avatarSource} style={{height:100}}/>} */}
-      <GiftedChat
-        messages={listChat}
-        onSend={(messages) => onSend(messages)}
-        text={text}
-        onInputTextChanged={setText}
-        user={{
-          _id: 1,
-        }}
-        renderInputToolbar={renderInputToolbar}
-        renderActions={renderActions}
-        renderComposer={renderComposer}
-        renderSend={renderSend}
-      />
-    </React.Fragment>
+    <GiftedChat
+      messages={listChat}
+      onSend={(messages) => onSend(messages)}
+      text={text}
+      onInputTextChanged={setText}
+      user={{
+        _id: 1,
+      }}
+      renderInputToolbar={renderInputToolbar}
+      renderActions={renderActions}
+      renderComposer={renderComposer}
+      renderSend={renderSend}
+    />
   );
 };
 export default App;
+const styles = StyleSheet.create({
+  actionContainer: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 4,
+    marginRight: 4,
+    marginBottom: 0,
+  },
+  caontainerToolbar: {
+    backgroundColor: '#222B45',
+    paddingTop: 6,
+  },
+  textInputBox: {
+    color: '#222B45',
+    backgroundColor: '#EDF1F7',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#E4E9F2',
+    paddingTop: 8.5,
+    paddingHorizontal: 12,
+    marginLeft: 0,
+  },
+  containerSend: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 4,
+  },
+});
